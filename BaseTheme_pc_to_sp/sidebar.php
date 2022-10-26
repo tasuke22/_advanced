e4f77ed88272f7ccead0e324121bd44491151006
tasuke22 <tasukesuke@gmail.com>
2022-10-27 07:35:16 +0900

    共通部分をまとめた

---
+++ BaseTheme_pc_to_sp/sidebar.php
@@ -0,0 +1,47 @@
+
+<div class="l-right">
+    <section class="l-related-article p-related-article">
+      <h2 class="p-related-article__title">関連記事</h2>
+      <?php
+      $terms = get_the_terms($post->ID, 'blog_category');
+      $blogCategoryRelatedPostData = get_posts([
+        'post_type' => 'blog',
+        'posts_per_page' => '3',
+        'post__not_in' => [$post->ID],
+        'orderby' => 'dest',
+        'tax_query' => [[
+          'taxonomy' => 'blog_category',
+          'field' => 'slug',
+          'terms' =>  [$terms[0]->slug],
+        ]],
+      ]);
+      foreach ($blogCategoryRelatedPostData as $post) :
+        setup_postdata($post);
+      ?>
+        <div class="p-related-article__wrap">
+          <div class="p-related-article__img">
+            <img src="<?php echo get_the_post_thumbnail_url();  ?>" alt="">
+          </div>
+          <div class="p-related-article__text"><a href="<?php the_permalink(); ?>"><?php the_content(); ?></a></div>
+        </div>
+      <?php
+      endforeach;
+      ?>
+    </section>
+
+
+    <section class="l-category p-category">
+      <h2 class="p-category__title">カテゴリー</h2>
+      <ul class="p-category__list">
+        <a href="">
+          <li class="p-category__item">カテゴリ1</li>
+        </a>
+        <a href="">
+          <li class="p-category__item">カテゴリ2</li>
+        </a>
+        <a href="">
+          <li class="p-category__item">カテゴリ3</li>
+        </a>
+      </ul>
+    </section>
+  </div>
--- BaseTheme_pc_to_sp/single-blog.php
+++ BaseTheme_pc_to_sp/single-blog.php
@@ -89,54 +89,7 @@
 
 
   </div>
-
-
-  <div class="l-right">
-    <section class="l-related-article p-related-article">
-      <h2 class="p-related-article__title">関連記事</h2>
-      <?php
-      $terms = get_the_terms($post->ID, 'blog_category');
-      $blogCategoryRelatedPostData = get_posts([
-        'post_type' => 'blog',
-        'posts_per_page' => '3',
-        'post__not_in' => [$post->ID],
-        'orderby' => 'dest',
-        'tax_query' => [[
-          'taxonomy' => 'blog_category',
-          'field' => 'slug',
-          'terms' =>  [$terms[0]->slug],
-        ]],
-      ]);
-      foreach ($blogCategoryRelatedPostData as $post) :
-        setup_postdata($post);
-      ?>
-        <div class="p-related-article__wrap">
-          <div class="p-related-article__img">
-            <img src="<?php echo get_the_post_thumbnail_url();  ?>" alt="">
-          </div>
-          <div class="p-related-article__text"><a href="<?php the_permalink(); ?>"><?php the_content(); ?></a></div>
-        </div>
-      <?php
-      endforeach;
-      ?>
-    </section>
-
-
-    <section class="l-category p-category">
-      <h2 class="p-category__title">カテゴリー</h2>
-      <ul class="p-category__list">
-        <a href="">
-          <li class="p-category__item">カテゴリ1</li>
-        </a>
-        <a href="">
-          <li class="p-category__item">カテゴリ2</li>
-        </a>
-        <a href="">
-          <li class="p-category__item">カテゴリ3</li>
-        </a>
-      </ul>
-    </section>
-  </div>
+  <?php get_sidebar(); ?>
 
 </div>
 
@@ -145,17 +98,7 @@
 
 
 <section class="l-detail-document p-document">
-  <div class="p-document__title">
-    まずは無料で資料請求から
-  </div>
-  <a href="contact.html">
-    <div class="c-request-button-document p-document__button">
-      資料請求
-    </div>
-  </a>
-  <a href="contact.html">
-    <div class="c-contact p-document__contact">お問い合わせ</div>
-  </a>
+  <?php get_template_part('includes/document-request'); ?>
 </section>
 
 <section class="p-contact">
